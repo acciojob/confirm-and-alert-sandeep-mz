4d6e295548c1cf32303d55b23f128f24a4ba70fb
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-04 20:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Display the confirmation dialog with the specified message
+const confirmation = confirm("Do you want to proceed?");
+
+// Check the user's response
+if (confirmation) {
+    // User clicked OK, show "Proceeding..." alert
+    alert("You clicked OK. Proceeding...");
+} else {
+    // User clicked Cancel, show "Exiting..." alert
+    alert("You clicked Cancel. Exiting...");
+}
